03e93d0d9e2b81677666bfdf006939e17faa7d15
HeLongaa <1946815225@qq.com>
2024-07-13 00:14:51 +0800

    功能更新：智能翻译
    Todo：关于页面；代码优化

--- app.py
+++ app.py
@@ -1,35 +1,37 @@
-# app.py
+import logging
+
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from chat import SparkApi
 from ocr.routes import ocr_bp
-from translation.routes import translate_bp  # 导入新的蓝图
+from translate.translation import translate_bp  # 导入新的蓝图
 from ppt.routes import ppt_bp
 from chat.routes import chat_bp, appid, api_key, api_secret, Spark_url, domain
-import logging
-
 logging.getLogger('werkzeug').disabled = True
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
 socketio = SocketIO(app)
 
 app.register_blueprint(ocr_bp, url_prefix='/ocr')
-app.register_blueprint(translate_bp, url_prefix='/translate')  # 注册新的蓝图
+app.register_blueprint(translate_bp, url_prefix='/tra')  # 注册新的蓝图
 app.register_blueprint(ppt_bp, url_prefix='/ppt')
 app.register_blueprint(chat_bp, url_prefix='/chat')
+
 
 @app.route('/')
 def index():
     return render_template('tool-main.html')
 
+
 @app.route('/tool')
 def tool():
     return render_template('Tools.html')
 
+
 @app.route('/tool-main')
 def toolmain():
     return render_template('tool-main.html')
+
 
 @socketio.on('message')
 def handle_message(data):
@@ -44,6 +46,7 @@ def handle_message(data):
             emit('response', {'content': content})
 
     SparkApi.main(appid, api_key, api_secret, Spark_url, domain, text, process_response)
+
 
 if __name__ == '__main__':
     socketio.run(app, allow_unsafe_werkzeug=True)
---
+++ translate/translation.py
@@ -0,0 +1,126 @@
+from flask import Blueprint, request, render_template, jsonify
+from datetime import datetime
+from wsgiref.handlers import format_date_time
+from time import mktime
+import hashlib
+import base64
+import hmac
+from urllib.parse import urlencode
+import json
+import requests
+
+translate_bp = Blueprint('translate', __name__, template_folder='templates', static_folder='static')
+
+APPId = "2d2fc3e9"
+APISecret = "NDQwZWJhNGU2MmNlOWJkOGQxYjI2ZTNi"
+APIKey = "f8930205f626138a4882b8d4e01a923b"
+RES_ID = "its_en_cn_word"
+
+
+class AssembleHeaderException(Exception):
+    def __init__(self, msg):
+        self.message = msg
+
+
+class Url:
+    def __init__(self, host, path, schema):
+        self.host = host
+        self.path = path
+        self.schema = schema
+
+
+def sha256base64(data):
+    sha256 = hashlib.sha256()
+    sha256.update(data)
+    digest = base64.b64encode(sha256.digest()).decode(encoding='utf-8')
+    return digest
+
+
+def parse_url(requset_url):
+    stidx = requset_url.index("://")
+    host = requset_url[stidx + 3:]
+    schema = requset_url[:stidx + 3]
+    edidx = host.index("/")
+    if edidx <= 0:
+        raise AssembleHeaderException("invalid request url:" + requset_url)
+    path = host[edidx:]
+    host = host[:edidx]
+    u = Url(host, path, schema)
+    return u
+
+
+def assemble_ws_auth_url(requset_url, method="POST", api_key="", api_secret=""):
+    u = parse_url(requset_url)
+    host = u.host
+    path = u.path
+    now = datetime.now()
+    date = format_date_time(mktime(now.timetuple()))
+    signature_origin = "host: {}\ndate: {}\n{} {} HTTP/1.1".format(host, date, method, path)
+    signature_sha = hmac.new(api_secret.encode('utf-8'), signature_origin.encode('utf-8'),
+                             digestmod=hashlib.sha256).digest()
+    signature_sha = base64.b64encode(signature_sha).decode(encoding='utf-8')
+    authorization_origin = "api_key=\"%s\", algorithm=\"%s\", headers=\"%s\", signature=\"%s\"" % (
+        api_key, "hmac-sha256", "host date request-line", signature_sha)
+    authorization = base64.b64encode(authorization_origin.encode('utf-8')).decode(encoding='utf-8')
+    values = {
+        "host": host,
+        "date": date,
+        "authorization": authorization
+    }
+    return requset_url + "?" + urlencode(values)
+
+
+@translate_bp.route('/')
+def index():
+    return render_template('index.html')
+
+
+@translate_bp.route('/translate', methods=['POST'])
+def translate():
+    text_to_translate = request.form['text']
+    from_language = request.form['from']
+    to_language = request.form['to']
+    url = 'https://itrans.xf-yun.com/v1/its'
+
+    body = {
+        "header": {
+            "app_id": APPId,
+            "status": 3,
+            "res_id": RES_ID
+        },
+        "parameter": {
+            "its": {
+                "from": from_language,
+                "to": to_language,
+                "result": {}
+            }
+        },
+        "payload": {
+            "input_data": {
+                "encoding": "utf8",
+                "status": 3,
+                "text": base64.b64encode(text_to_translate.encode("utf-8")).decode('utf-8')
+            }
+        }
+    }
+
+    request_url = assemble_ws_auth_url(url, "POST", APIKey, APISecret)
+    headers = {'content-type': "application/json", 'host': 'itrans.xf-yun.com', 'app_id': APPId}
+
+    response = requests.post(request_url, data=json.dumps(body), headers=headers)
+
+    if response.status_code == 200:
+        tempResult = json.loads(response.content.decode())
+        translated_text_base64 = tempResult['payload']['result']['text']
+        translated_text = base64.b64decode(translated_text_base64).decode()
+        translated_text_json = json.loads(translated_text)
+        translated_text_result = translated_text_json['trans_result']['dst']
+        result = {
+            'headers': headers,
+            'response': str(response),
+            'content': response.content.decode(),
+            'translated_text': translated_text_result
+        }
+        return jsonify(result)
+    else:
+        return jsonify({'error': 'Translation failed'}), 500
